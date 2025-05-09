693299c24d3a770e929349131aa0834fc0359e4e
FukenMike <mikegriggs929@gmail.com>
2025-05-09 12:13:29 -0400

    Delete App.tsx

--- App.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function App() {
-  return (
-    <div className="text-center text-2xl text-amber-500 p-8">
-      Welcome to The Father’s Alliance - Redemption 2.0 Build
-    </div>
-  );
-}
